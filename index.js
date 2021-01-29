07f25386435a3a3f6d8599e686aa9c9679b3c064
GrzegorzNecka <strzypior@gmail.com>
2021-01-29 20:06:17 +0100

    1 file modified

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import "./style.css";
 import appendItems from "./appendItems";
 
 const container = document.querySelector(".w-full.container");
+const btns = document.querySelector("ul");
 
 const contactList = [
   "Peter Petrelli",
@@ -19,3 +20,9 @@ const page = 1;
 const recordsPerPage = 2;
 
 appendItems(page, recordsPerPage, contactList, container);
+
+btns.addEventListener("click", ({ target }) => {
+  console.log(target.innerText);
+  console.log(parseInt(target.innerText));
+  console.log(isNaN(parseInt(target.innerText)));
+});
