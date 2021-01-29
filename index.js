fcaf2fbe1002c02af19a9d1e1d497da91a12e4bc
GrzegorzNecka <strzypior@gmail.com>
2021-01-29 20:23:42 +0100

    1 file modified

--- index.js
+++ index.js
@@ -18,10 +18,33 @@ const contactList = [
 
 const page = 1;
 const recordsPerPage = 2;
+let paginationCounter = page;
 
 appendItems(page, recordsPerPage, contactList, container);
 
+function usePagination(currentPage) {
+  container.innerText = "";
+  appendItems(currentPage, recordsPerPage, contactList, container);
+  paginationCounter = currentPage;
+}
+
 btns.addEventListener("click", ({ target }) => {
+  if (!isNaN(parseInt(target.innerText))) {
+    usePagination(parseInt(target.innerText));
+  } else if (target.innerText === "Previous") {
+    if (paginationCounter >= 0) {
+      paginationCounter--;
+      usePagination(paginationCounter--);
+    }
+  } else if (target.innerText === "Next") {
+    const maxLength = btns.childElementCount - 2;
+    if (paginationCounter <= maxLength) {
+      paginationCounter++;
+      usePagination(paginationCounter++);
+    }
+  }
+
+  console.log();
   console.log(target.innerText);
   console.log(parseInt(target.innerText));
   console.log(isNaN(parseInt(target.innerText)));
